49801d9c0b41a43bd2dad0b39d256193d991c6f2
Victoriabunmi72 <bhummhie97@gmail.com>
2023-05-06 17:30:23 +0100

    2-puppet_custom_http_response_header.pp

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# 2-puppet_custom_http_response_header.pp
+
+exec {'update':
+	provider => shell,
+	command  => 'sudo apt-get -y update',
+	before   => Exec['install nginx'],
+
+}
+
+exec {'install nginx':
+	provider => shell,
+	command  => 'sudo apt-get -y install nginx',
+	before   => Exec['add_header'],
+
+}
+
+exec {'add_header':
+	provider     => shell,
+	environment  => ["HOST=${HOSTNAME}"],
+	command      => 'echo "add_header X-Served-By $HOSTNAME;" | sudo tee -a /etc/nginx/nginx.conf > /dev/null',
+
+}
+
+exec {'restart nginx':
+	provider  => shell,
+	command   => 'sudo service nginx restart',
+
+}
