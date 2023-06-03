46d203c67679e8f0c3c0b389713907ca2de1c896
Victoriabunmi72 <bhummhie97@gmail.com>
2023-06-03 23:53:03 +0100

    1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""static/scripts/1-hbnb.js"""
+
+import requests
+
+def top_ten(subreddit):
+    """
+    Print the titles of the 10 hottest posts on a given subreddit
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+
+    data = requests.get(url, headers={'User-agent': 'Dragneel'},
+                        allow_redirects=False)
+    if data.status_code == 200:
+        post_list = data.json().get('data').get('children')
+        count = 0
+        for post in post_list:
+            if count == 10:
+                break
+            print(post.get("data").get("title"))
+            count = count + 1
+    else:
+        print("None")
